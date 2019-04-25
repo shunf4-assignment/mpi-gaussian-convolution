db8df6c742c7a53a7653f5e9cf635d4388ccc1a9
shunf4 <shun1048576@gmail.com>
2019-04-25 21:35:14 +0800

    Fix run script

--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-mpiexec -n 1 vs\MPI\x64\Debug\MPI.exe test.bmp output1.bmp
-mpiexec -n 2 vs\MPI\x64\Debug\MPI.exe test.bmp output2.bmp
-mpiexec -n 3 vs\MPI\x64\Debug\MPI.exe test.bmp output3.bmp
-mpiexec -n 4 vs\MPI\x64\Debug\MPI.exe test.bmp output4.bmp
-mpiexec -n 8 vs\MPI\x64\Debug\MPI.exe test.bmp output8.bmp
+mpiexec -n 1 ./_mpi test.bmp output1.bmp
+mpiexec -n 2 ./_mpi test.bmp output2.bmp
+mpiexec -n 3 ./_mpi test.bmp output3.bmp
+mpiexec -n 4 ./_mpi test.bmp output4.bmp
+mpiexec -n 6 ./_mpi test.bmp output6.bmp
+mpiexec -n 8 ./_mpi test.bmp output8.bmp
 diff -s output1.bmp output2.bmp
 diff -s output1.bmp output3.bmp
 diff -s output1.bmp output4.bmp
+diff -s output1.bmp output6.bmp
 diff -s output1.bmp output8.bmp
