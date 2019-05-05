4a0c0794720af03aaadf2fc93320a2b8905eaee9
shunf4 <1652270@tongji.edu.cn>
2019-05-05 21:54:49 +0800

    Update runtest.sh

--- runtest.sh
+++ runtest.sh
@@ -1,4 +1,8 @@
 #!/bin/bash -x
+if [ "x$1x" == "xx" ]; then
+	echo "Usage: $0 <executable_file_without_underline>"
+	exit 1;
+fi
 
 time mpiexec -n 4 ./_$1 timg.bmp output_$1.bmp
 ./newvalid timg.bmp output_$1.bmp
